9283679fb0e3a8246d3326f6fba429627dba34c4
angrylobster <liewminshan@gmail.com>
2019-03-14 23:16:19 +0800

    Setup basic files

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComments() {
+    fetch('http://localhost:3000/comments')
+        .then(response => {
+            return response.json();
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        .then(json => {
+            console.log(json[0]);
+        })
+}
